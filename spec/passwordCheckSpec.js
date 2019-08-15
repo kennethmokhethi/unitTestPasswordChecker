4d46db3ebbe6c2d108537744c144ba3b5b65ecdf
kennethmokhethi <kennethmahlatsemokhethi@gmail.com>
2019-08-15 11:49:10 +0200

    Tested the null option

--- spec/passwordCheckSpec.js
+++ spec/passwordCheckSpec.js
@@ -1,8 +1,16 @@
 describe("Password checker",()=>{
+  it("Check for null password",()=>{
+    var password=null;
+    var ansP=passwordCheck(password);
+    expect(ansP).toBe("Error:The password is null");
+
+
+  })
+ 
   it("Check for characters length",()=>{
-      var passW="hagsbx";
-      var ansP=passwordCheck(passW);
-      expect(ansP).toBe("Error:The lengh of the password is not equal to 8 characters");
+      var password="hagsbx";
+      var ansP=passwordCheck(password);
+      expect(ansP).toBe("Error:The password needs to have 8 or more characters,atleast one upper case letter and atleast a number");
   })
 
 

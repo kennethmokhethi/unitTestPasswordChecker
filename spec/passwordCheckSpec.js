c9350f330aaf6de026c53058233e24690e2c68e0
kennethmokhethi <kennethmahlatsemokhethi@gmail.com>
2019-08-15 11:57:40 +0200

    Tested the password for uppercase and lowercase

--- spec/passwordCheckSpec.js
+++ spec/passwordCheckSpec.js
@@ -5,13 +5,25 @@ describe("Password checker",()=>{
     expect(ansP).toBe("Error:The password is null");
 
 
-  })
+  });
  
   it("Check for characters length",()=>{
       var password="hagsbx";
       var ansP=passwordCheck(password);
       expect(ansP).toBe("Error:The password needs to have 8 or more characters,atleast one upper case letter and atleast a number");
-  })
+  });
+
+  it("Check upper case letters",()=>{
+    var password="hsgd2hsg";
+    var ansP=passwordCheck(password);
+    expect(ansP).toBe("Error:The password need to contain atleast one uppercase letter")
+  });
+
+  it("Check for lower case letter",()=>{
+    var password="HDCGDB3H";
+    var ansP=passwordCheck(password);
+    expect(ansP).toBe("Error:The password need to contain atleast one lower case letter")
+  });
 
 
-})
+});

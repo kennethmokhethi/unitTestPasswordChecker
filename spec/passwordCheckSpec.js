8768f630b7628be8ffc1bda11429525a415a7fe2
Kenneth Mahlatse Mokhethi <kennethmahlatsemokhethi@gmail.com>
2019-09-12 12:23:15 +0200

    Addeded error handling and remove repitative lines

--- spec/passwordCheckSpec.js
+++ spec/passwordCheckSpec.js
@@ -1,64 +1,37 @@
 describe("Password checker.One condition is not met", () => {
   it("Check for null password", () => {
     var password = null;
-    var ansP = passwordCheck(password);
-    expect(ansP).toBe("Error:The password is null");
+    var ansP = password_is_valid(password);
+    expect(ansP).toBe("Error: The password is null");
   });
 
   it("Check upper case letters", () => {
     var password = "hsgd2hsg";
-    var ansP = passwordCheck(password);
-    expect(ansP).toBe("Error:The password need to contain atleast one uppercase letter")
+    var ansP = password_is_valid(password);
+    expect(ansP).toBe("Error: The password need to contain atleast one uppercase letter")
   });
 
   it("Check for lower case letter", () => {
     var password = "HDCGDB3H";
-    var ansP = passwordCheck(password);
-    expect(ansP).toBe("Error:The password need to contain atleast one lower case letter")
+    var ansP = password_is_valid(password);
+    expect(ansP).toBe("Error: The password need to contain atleast one lower case letter")
   });
 
   it("Check for numbers", () => {
     var password = "Hgafsvgt";
-    var ansP = passwordCheck(password);
-    expect(ansP).toBe("Error:The password needs to contain atleast one number")
+    var ansP = password_is_valid(password);
+    expect(ansP).toBe("Error: The password needs to contain atleast one number")
   });
+
+ it("Password meets the first two conditions",()=>{
+  var password = "Hgafsvgt";
+  var ansP = password_is_ok(password);
+  expect(ansP).toBe(true);
+
+ });
+
+
+
 });
 
 
-describe("Password checker.Multiply conditions are not met", () => {
-  it("Has lower cases only", () => {
-    var password = "gdghs";
-    var ansP = passwordCheck(password);
-    expect(ansP).toBe("Error:The password needs to have 8 or more characters,atleast one upper case letter and atleast a number")
-  });
-
-  it("Has uppercase only", () => {
-    var password = "HAFSV";
-    var ansP = passwordCheck(password);
-    expect(ansP).toBe("Error:The password needs to have 8 or more characters, atleast a lower case letter and atleast a number")
-  });
-
-  it("Password with characters less than 8 and no lower case", () => {
-    var password = "H2K3";
-    var ansP = passwordCheck(password);
-    expect(ansP).toBe("Error:The password needs to have 8 or more characters and atleast a lower case letter")
-  });
-
-  it("Passwords that has numbers only", () => {
-    var password = "123456789";
-    var ansP = passwordCheck(password);
-    expect(ansP).toBe("Error:The password needs to have atleast a upper and lower case letters")
-  });
-
-  it("Passwords that has numbers only with characters less than 8", () => {
-    var password = "12345";
-    var ansP = passwordCheck(password);
-    expect(ansP).toBe("Error:The password needs to have 8 or more charecters,atleast a upper and lower case letters")
-  });
-
-  it("Password with lower case letters, a number and characters less than 8", () => {
-    var password = "gafs2";
-    var ansP = passwordCheck(password);
-    expect(ansP).toBe("Error:The password needs to have 8 or more characters and have atleast an upper case letter")
-  });
-});
--- src/passwordChecker.js
+++ src/passwordChecker.js
@@ -1,6 +1,9 @@
+
 //function that validates password to ensure that all requirements of a password are met
-function passwordCheck(password) {
-     //    Deaclaration and iniatialisation of varibales 
+
+var conditionsMet=0;
+function password_is_valid(password) {
+   
      var passLength = false;
      var passUpperCase = false;
      var passLowerCase = false;
@@ -10,79 +13,84 @@ function passwordCheck(password) {
      var lowerCaseLetters = /[a-z]/g;
      var numbers = /[0-9]/g;
      var status;
-
+   
      // Setting the conditions
      if (password != null) {
           passNul = false;
-
+   
           if (password.length >= 8) {
                passLength = true;
           }
-
+   
           if (password.match(upperCaseLetters)) {
                passUpperCase = true;
           }
-
+   
           if (password.match(lowerCaseLetters)) {
                passLowerCase = true;
           }
-
+   
           if (password.match(numbers)) {
                passNum = true;
           }
      }
+    
+    //Error handling
+    try{
+         if(passNul==false )
+         {
+          conditionsMet+=2;
+              if(passLength==true)
+              {
+               conditionsMet+=2;
+                   if(passUpperCase==true)
+                   {
+                    ++conditionsMet;
+                        if(passLowerCase==true)
+                        {
+                         ++conditionsMet;
+                          if(passNum==true)
+                          {
+                              ++conditionsMet
 
+                          }else{
+                              throw "The password needs to contain atleast one number";
+                          }
+                        }else{
+                         throw "The password need to contain atleast one lower case letter";
+                        }
 
-     //Error handling
-      try{
+                   }else{
+                    throw "The password need to contain atleast one uppercase letter";
+                   }
 
-          if (passNul == true) throw "Error:The password is null";
-          if ((passNul == false) && (passLength == false) && (passUpperCase == true) && (passLowerCase == true) && (passNum == true)) throw "The lengh of the password is less than to 8 characters";
-          if ((passNul == false) && (passLength == true) && (passUpperCase == false) && (passLowerCase == true) && (passNum == true)) throw "The password need to contain atleast one uppercase letter";
-          if ((passNul == false) && (passLength == true) && (passUpperCase == true) && (passLowerCase == false) && (passNum == true)) throw "The password need to contain atleast one lower case letter";
-          if ((passNul == false) && (passLength == true) && (passUpperCase == true) && (passLowerCase == true) && (passNum == false)) throw "The password needs to contain atleast one number";
-          if ((passNul == false) && (passLength == false) && (passUpperCase == false) && (passLowerCase == true) && (passNum == false)) throw "The password needs to have 8 or more characters,atleast one upper case letter and atleast a number";
-          if ((passNul == false) && (passLength == false) && (passUpperCase == true) && (passLowerCase == false) && (passNum == false)) throw "The password needs to have 8 or more characters, atleast a lower case letter and atleast a number";
-          if ((passNul == false) && (passLength == false) && (passUpperCase == true) && (passLowerCase == false) && (passNum == true)) throw "The password needs to have 8 or more characters and atleast a lower case letter"
-          if ((passNul == false) && (passLength == true) && (passUpperCase == false) && (passLowerCase == false) && (passNum == true)) throw "The password needs to have atleast a upper and lower case letters";
-          if ((passNul == false) && (passLength == false) && (passUpperCase == false) && (passLowerCase == false) && (passNum == true)) throw "The password needs to have 8 or more charecters,atleast a upper and lower case letters";
-          if ((passNul == false) && (passLength == false) && (passUpperCase == false) && (passLowerCase == true) && (passNum == true)) throw status = "The password needs to have 8 or more characters and have atleast an upper case letter";
-     
-     }catch(err){
-          return "Error:" + err;
-     }
+              }else{
+               throw "The lengh of the password is less than to 8 characters";
+              }
 
+         }else{
+          throw "The password is null";
+         }
 
+    }catch(err){
+       return "Error: " + err;
 
-     
+    }
 
-     // // Return status if a specific conndition is met
-     // if ((passNul == true)) {
-     //      status = "Error:The password is null";
-     // } else if ((passNul == false) && (passLength == false) && (passUpperCase == true) && (passLowerCase == true) && (passNum == true)) {
-     //      status = "Error:The lengh of the password is less than to 8 characters";
-     // } else if ((passNul == false) && (passLength == true) && (passUpperCase == false) && (passLowerCase == true) && (passNum == true)) {
-     //      status = "Error:The password need to contain atleast one uppercase letter";
-     // } else if ((passNul == false) && (passLength == true) && (passUpperCase == true) && (passLowerCase == false) && (passNum == true)) {
-     //      status = "Error:The password need to contain atleast one lower case letter";
-     // } else if ((passNul == false) && (passLength == true) && (passUpperCase == true) && (passLowerCase == true) && (passNum == false)) {
-     //      status = "Error:The password needs to contain atleast one number";
-     // } else if ((passNul == false) && (passLength == false) && (passUpperCase == false) && (passLowerCase == true) && (passNum == false)) {
-     //      status = "Error:The password needs to have 8 or more characters,atleast one upper case letter and atleast a number";
-     // } else if ((passNul == false) && (passLength == false) && (passUpperCase == true) && (passLowerCase == false) && (passNum == false)) {
-     //      status = "Error:The password needs to have 8 or more characters, atleast a lower case letter and atleast a number";
-     // } else if ((passNul == false) && (passLength == false) && (passUpperCase == true) && (passLowerCase == false) && (passNum == true)) {
-     //      status = "Error:The password needs to have 8 or more characters and atleast a lower case letter"
-     // } else if ((passNul == false) && (passLength == true) && (passUpperCase == false) && (passLowerCase == false) && (passNum == true)) {
-     //      status = "Error:The password needs to have atleast a upper and lower case letters"
-     // } else if ((passNul == false) && (passLength == false) && (passUpperCase == false) && (passLowerCase == false) && (passNum == true)) {
-     //      status = "Error:The password needs to have 8 or more charecters,atleast a upper and lower case letters"
-     // } else if ((passNul == false) && (passLength == false) && (passUpperCase == false) && (passLowerCase == true) && (passNum == true)) {
-     //      status = "Error:The password needs to have 8 or more characters and have atleast an upper case letter";
-     // } else {
-     //      status = "All requirements were met";
-     // }
-
-     return status;
+     return conditionsMet;
 
 }
+
+///If the 3 conditions are met then the function will return true
+
+function password_is_ok(password)
+{
+   
+   var numConditionMet=password_is_valid(password);
+
+   if((numConditionMet<4)) return false;
+    
+  return true
+   
+}
+// console.log(function_is_ok("gdfrt21"))
